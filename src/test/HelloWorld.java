810bec662eebacf7cb6422a2487f33f41624c203
ethan <ethshoe@iastate.edu>
2018-01-22 17:54:44 -0600

    More Spring Tests

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -9,4 +9,10 @@ public class HelloWorld {
     public void getMessage(){
         System.out.println("Your Message : " + message);
     }
+    public void init() {
+        System.out.println("I'm getting ready");
+    }
+    public void destroy() {
+        System.out.println("Oh fuck there I go");
+    }
 }
--- src/test/MainApp.java
+++ src/test/MainApp.java
@@ -1,16 +1,15 @@
 package test;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
         objA.setMessage("It's me fucker");
         objA.getMessage();
-
-        HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
-        objB.getMessage();
+        context.registerShutdownHook();
     }
 }
