fdd42081ac3fb84e0f42d88b511befa2ac0b9725
ethan <ethshoe@iastate.edu>
2018-01-25 22:59:45 -0600

    Added Hibernate preliminary

---
+++ src/test/UserRepository.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.springframework.data.repository.CrudRepository;
+
+import test.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
