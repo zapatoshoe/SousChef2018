e44d90bdda7f2da7f3c51427d29df6c8a42efc0d
ethan <ethshoe@iastate.edu>
2018-01-28 14:22:39 -0600

    Embedded Repository functionality

---
+++ DatabaseDummy/src/main/java/db/app/Person/PersonService.java
@@ -0,0 +1,39 @@
+package db.app.Person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class PersonService {
+	@Autowired
+	private PersonRepository personRepository;
+
+	public Person getPerson(String name) {
+		return personRepository.findOne(name);
+	}
+
+	public List<Person> getAllPersons() {
+		List<Person> l = new ArrayList<>();
+		personRepository.findAll().forEach(l::add);
+		return l;
+	}
+
+	public void addPerson(Person person) {
+		personRepository.save(person);
+	}
+
+	public void updatePerson(Person person, String name) {
+		personRepository.save(person);	//same as add but repository knows to update existing rows
+	}
+
+	public void deletePerson(String name) {
+		personRepository.delete(name);
+	}
+
+}
---
+++ DatabaseDummy/src/test/java/db/app/DatabaseDummyApplicationTests.java
@@ -0,0 +1,16 @@
+package db.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatabaseDummyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
