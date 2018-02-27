c89d7f8a4c10dc443e3ec542caa10c5f4c6888ed
Danny <seegerd@iastate.edu>
2018-02-26 21:39:20 -0600

    Implemented Recipe Steps. All functionality should be present

--- SousChefServer/src/main/java/db/app/RecipeSteps/RecipeStepsController.java
+++ SousChefServer/src/main/java/db/app/RecipeSteps/RecipeStepsController.java
@@ -3,6 +3,7 @@ package db.app.RecipeSteps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
 @RestController
@@ -12,13 +13,38 @@ public class RecipeStepsController {
     @Autowired
     RecipeStepsService recipeStepsService;
 
-    @RequestMapping("/{recipeId}")
-    public List<RecipeSteps> getRecipeSteps(@PathVariable Integer recipeId){
-        recipeStepsService.getRecipeSteps(recipeId);
+    @RequestMapping("/{recipeStepId}")
+    public RecipeSteps getStep(@PathVariable Integer recipeStepId){
+        return recipeStepsService.getStep(recipeStepId);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/{recipeId}")
+    @RequestMapping("/recipe/{recipeId}")
+    public List<RecipeSteps> getRecipeSteps(@PathVariable Integer recipeId){
+        return recipeStepsService.getRecipeSteps(recipeId);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/recipe/{recipeId}")
     public void addRecipeStep(@PathVariable Integer recipeId, @RequestBody RecipeSteps recipeSteps){
         recipeStepsService.addRecipeStep(recipeId, recipeSteps);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/recipe/{recipeId}/{stepNumber}")
+    public void addStepToRecipe(@PathVariable Integer recipeId, @PathVariable Integer stepNumber, @RequestBody RecipeSteps step){
+        recipeStepsService.addStepToRecipe(recipeId, stepNumber, step);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/{recipeStepId}")
+    public void updateStep(@PathVariable Integer recipeStepId, @RequestBody RecipeSteps newStep){
+        recipeStepsService.updateStep(recipeStepId, newStep);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/recipe/{recipeId}")
+    public void deleteRecipeSteps(@PathVariable Integer recipeId){
+        recipeStepsService.deleteRecipeSteps(recipeId);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{recipeStepId}")
+    public void deleteStep(@PathVariable Integer recipeStepId){
+        recipeStepsService.deleteStep(recipeStepId);
     }
 }
--- SousChefServer/src/main/java/db/app/RecipeSteps/RecipeStepsRepository.java
+++ SousChefServer/src/main/java/db/app/RecipeSteps/RecipeStepsRepository.java
@@ -1,6 +1,11 @@
 package db.app.RecipeSteps;
 
+import db.app.Recipe.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RecipeStepsRepository extends CrudRepository<RecipeSteps, Integer> {
+    List<RecipeSteps> findByRecipe(Recipe recipe);
+
 }
