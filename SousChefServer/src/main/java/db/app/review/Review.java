b3dfe545b119b7ec89bc7f8dd6c10d312715d9dc
Danny <seegerd@iastate.edu>
2018-03-19 20:09:21 -0500

    Review skeleton

--- SousChefServer/src/main/java/db/app/review/Review.java
+++ SousChefServer/src/main/java/db/app/review/Review.java
@@ -1,8 +1,10 @@
 package db.app.review;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import db.app.person.Person;
+import db.app.recipe.Recipe;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Review {
@@ -11,8 +13,90 @@ public class Review {
     @GeneratedValue
     private Integer id;
 
+    @ManyToOne
+    private Person owner;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    private Integer rating;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private Date date;
     //TODO increment and decrement the Recipe's numReviews
     //TODO update average rating
     //TODO Creation Date, see Person or Recipe class for how to store current Date
 
+    public Review(){
+
+    }
+
+    public Review(Person owner, Recipe recipe, Integer rating, String title, String description, Date date) {
+        this.owner = owner;
+        this.recipe = recipe;
+        this.rating = rating;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
---
+++ SousChefServer/src/main/java/db/app/review/ReviewController.java
@@ -0,0 +1,4 @@
+package db.app.review;
+
+public class ReviewController {
+}
